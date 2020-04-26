ba4a09565672d8ae07625f1c605bbb1f30082057
buddi56 <ynaveenk.0513@gmail.com>
2020-04-26 21:26:51 +0530

    updated with DT changes

--- Android.mk
+++ Android.mk
@@ -18,4 +18,15 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),CPH1859)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := SoterService
+LOCAL_MODULE_OWNER := oppo
+LOCAL_SRC_FILES := proprietary/app/SoterService/SoterService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
 endif
